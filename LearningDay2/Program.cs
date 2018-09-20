18eaf7dbcd1dde109deccaa9800e1559ecf6b5dc
Misiak <Misiak@DESKTOP-NGERQUU>
2018-09-20 15:17:03 +0200

    Utworzenie metod odpowiedzialnych za podawanie imienia, nazwiska oraz za oczyszczanie consoli. Metoda odpowiedzialna za podawanie wieku ma zabespieczenia chroniące ją przed podaniem liter.

--- LearningDay2/Program.cs
+++ LearningDay2/Program.cs
@@ -12,8 +12,72 @@ namespace LearningDay2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("sadas");
+            for (; ; )
+            {
 
+
+                PodajImie();
+                PodajWiek();
+                KoniecProgramu();
+            }
+
+        }
+
+        /// <summary>
+        /// Metoda czyszczaca ekran
+        /// </summary>
+        private static void KoniecProgramu()
+        {
+            Console.ReadKey(); // czeka aż użytkownik wciśnie klawisz
+            Console.Clear();
+            Console.ResetColor();
+        }
+
+        /// <summary>
+        /// Metoda odpowiedzialna za wprowadzenie wieku użytkownika
+        /// </summary>
+        private static void PodajWiek()
+        {
+            Console.Write("your age? ");
+            int age;
+            bool CzyZgodnyWiek = int.TryParse(Console.ReadLine(), out age); //Konwertuje sting do inta sprawdzajac przy tym czy podajemy liczbe czy litere
+
+
+            if (CzyZgodnyWiek == true)
+            {
+                if (age >= 18)
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("Jesteś pełnoletni");
+                }
+                else
+                {
+                    Console.WriteLine("Jesteś niepełnoletni");
+                }
+
+            }
+            else
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Podałeś zły wiek spróbuj jeszcze raz !");
+                    Console.ResetColor();
+
+                    PodajWiek();
+                }
+        
+
+    
+        }
+
+
+        /// <summary>
+        /// Metoda odpowiedzialna za wprowadzenie imienia użytkownika.
+        /// </summary>
+        private static void PodajImie()
+        {
+            Console.Write("your name? ");
+            string name = Console.ReadLine(); // method downolanding date, name
+            Console.WriteLine("Hallo " + name);
         }
     }
 }
