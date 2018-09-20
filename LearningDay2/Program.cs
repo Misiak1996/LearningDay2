a5a2d34e62e25d58dbf44d5c93d3af7c0c92ae95
Misiak <Misiak@DESKTOP-NGERQUU>
2018-09-20 14:11:57 +0200

    writeLine

--- LearningDay2/Program.cs
+++ LearningDay2/Program.cs
@@ -3,13 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+/// <summary>
+/// Learning day 2
+/// </summary>
 namespace LearningDay2
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("sadas");
+
         }
     }
 }
